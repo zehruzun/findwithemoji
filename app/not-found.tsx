db84a1563c2d5c175bf8e57953449cc43d3e261e
Zehra Uzun <zehruzun@hotmail.com>
2025-08-25 03:11:56 +0300

    d

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold">404 - Sayfa bulunamadı</h1>
+      </div>
+    );
+  }
+  
